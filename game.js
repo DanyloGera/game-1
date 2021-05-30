3712d853a4b6c89752cce56909a5c381034d9b05
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-30 11:18:17 +0300

    Update game.js

--- game.js
+++ game.js
@@ -113,6 +113,7 @@ window.onload = function () {
   // before the game begins
   function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
       game.scale.setMinMax(800, 600, 900, 700);
     // game.stage.backgroundColor = '#5db1ad';
     game.load.image('background', 'background.png');
