3a4aad02359af4ebf6b1940a171cdae0d9ecfced
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-30 10:06:58 +0300

    Update game.js

--- game.js
+++ game.js
@@ -109,9 +109,10 @@ function badgeHandler(player, badge) {
 // setup game when the web page loads
 window.onload = function () {
   game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-game.style.width = "60%";
+
   // before the game begins
   function preload() {
+    game.style.width = "60%";
     // game.stage.backgroundColor = '#5db1ad';
     game.load.image('background', 'background.png');
 
