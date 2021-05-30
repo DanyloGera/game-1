c146a69bad0ad1e9dd4db228070f9a2d5f70f916
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-30 10:00:56 +0300

    Update game.js

--- game.js
+++ game.js
@@ -108,7 +108,7 @@ function badgeHandler(player, badge) {
 
 // setup game when the web page loads
 window.onload = function () {
-  game = new Phaser.Game( Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
   // before the game begins
   function preload() {
