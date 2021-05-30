d1824790f8e7ad82f1859b66f419666692fe5363
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-05-30 11:32:43 +0300

    Update game.js

--- game.js
+++ game.js
@@ -113,9 +113,9 @@ window.onload = function () {
   // before the game begins
   function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    game.scale.setMinMax(700, 500, 900, 600);
+    game.scale.setMinMax(700, 500, 900, 700);
  game.scale.pageAlignHorizontally = true;
- game.scale.pageAlignVertically = true;
+ // game.scale.pageAlignVertically = true;
     // game.stage.backgroundColor = '#5db1ad';
     game.load.image('background', 'background.png');
 
